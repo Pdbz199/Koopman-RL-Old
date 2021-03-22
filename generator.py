f6b17b0d6002708db3f3587b6d897a2565c88130
Preston Rozwood <279pdbz199@gmail.com>
2021-03-22 00:21:51 -0700

    Some updates on generator procedure

--- generator.py
+++ generator.py
@@ -1,31 +1,30 @@
 import numpy as np
+import scipy as sp
 
 '''======================= HELPER FUNCTIONS ======================='''
 
 # Construct B matrix as seen in 3.1.2 of the reference paper
 def constructB(d, n):
     Bt = np.zeros((d, n))
-    if Bt.shape[1] == 1:
+    if n == 1:
         Bt[0,0] = 1
     else:
-        row = 0
-        for i in range(1, d+1):
-            Bt[row,i] = 1
-            row += 1
-        B = np.transpose(Bt)
+        num = np.arange(d)
+        Bt[num, num+1] = 1
+    B = Bt.T
     return B
 
 # Construct similar B matrix as above, but for second order monomials
 def constructSecondOrderB(s, n):
     Bt = np.zeros((s, n))
-    if Bt.shape[1] == 1:
+    if n == 1:
         Bt[0,0] = 1
     else:
         row = 0
         for i in range(d+1, d+1+s):
             Bt[row,i] = 1
             row += 1
-    B = np.transpose(Bt)
+    B = Bt.T
     return B
 
 '''======================= CONSTRUCT DATA MATRIX ======================='''
@@ -37,7 +36,7 @@ delta = 2
 # Total time.
 T = 10.0
 # Number of steps.
-N = 500
+N = 1000
 # Time step size
 dt = T/N
 # Number of realizations to generate.
@@ -51,21 +50,28 @@ brownian(X[:, 0], N, dt, delta, out=X[:, 1:])
 '''======================= SETUP/DEFINITIONS ======================='''
 #%% 
 import observables
+from sympy import symbols
+from sympy.polys.monomials import itermonomials, monomial_count
+from sympy.polys.orderings import monomial_key
 
 d = X.shape[0]
 m = X.shape[1]
 s = int(d*(d+1)/2) # number of second order poly terms
-psi = observables.monomials(3)
+psi = observables.monomials(2)
+x_str = ""
+for i in range(d):
+    x_str += 'x_' + str(i) + ', '
+x_syms = symbols(x_str)
+M = itermonomials(x_syms, 2)
+sortedM = sorted(M, key=monomial_key('grlex', np.flip(x_syms)))
+# print(sortedM)
 Psi_X = psi(X)
 nablaPsi = psi.diff(X)
 nabla2Psi = psi.ddiff(X)
+print("nablaPsi Shape", nablaPsi.shape)
 n = Psi_X.shape[0]
 #%%
 '''======================= COMPUTATIONS ======================='''
-
-from sympy import symbols
-from sympy.polys.monomials import itermonomials, monomial_count
-from sympy.polys.orderings import monomial_key
 
 # This computes dpsi_k(x) exactly as in the paper
 # t = 1 is a placeholder time step, not really sure what it should be
@@ -95,46 +101,54 @@ B = constructB(d, n)
 # Computed b function (sometimes called \mu)
 L_times_B_transposed = (L @ B).T
 def b(l):
-    return L_times_B_transposed @ Psi_X[:, l]
+    return L_times_B_transposed @ Psi_X[:, l] # (k,)
 
-# Check array of b's
-count = 0
-for l in range(m):
-    comp = b(l)
-    count += abs(np.sum(comp)) > 5
-print(count) # 97
+def check_symmetric(a, rtol=1e-02, atol=1e-02):
+    return np.allclose(a, a.T, rtol=rtol, atol=atol)
+print("Is the matrix approximately symmetric:", check_symmetric(L))
 
-# # Eigen decomposition
-# eig_vals, eig_vecs = np.linalg.eigh(L)
-# # Calculate Koopman modes
-# V = np.transpose(B) @ np.linalg.inv((eig_vecs).T)
-# # Compute eigenfunction matrix
-# eig_funcs = (eig_vecs).T @ Psi_X
+# Eigen decomposition
+eig_vals, eig_vecs = sp.sparse.linalg.eigs(L) if sp.sparse.issparse(L) else sp.linalg.eig(L)
+# Calculate Koopman modes
+V = B.T @ np.linalg.inv((eig_vecs).T)
+# Compute eigenfunction matrix
+eig_funcs = (eig_vecs).T @ Psi_X
 
-# # This b function allows for heavy dimension reduction!
-# # default is reducing by 90% (taking the first n/10 eigen-parts)
-# def b_v2(l, num_dims=n//10):
-#     res = 0
-#     for ell in range(n-1, n-num_dims, -1):
-#         res += eig_vals[ell].reshape(-1, 1) * eig_funcs[ell, l].reshape(-1, 1) * V[:, ell].reshape(-1, 1)
-#     return res
+# This b function allows for heavy dimension reduction!
+# default is reducing by 90% (taking the first n/10 eigen-parts)
+# TODO: Figure out correct place to take reals
+def b_v2(l, num_dims=n//10):
+    res = 0
+    for ell in range(n-1, n-num_dims, -1):
+        res += eig_vals[ell] * eig_funcs[ell, l] * V[:, ell] #.reshape(-1, 1)
+    return np.real(res)
 
-# # Construct second order B matrix (selects second-order monomials)
-# second_orderB = constructSecondOrderB(s, n)
+# Construct second order B matrix (selects second-order monomials)
+second_orderB = constructSecondOrderB(s, n)
 
-# # the a function
-# # this was calculated in a weird way, so could have issues...
-# def a(l):
-#     return (np.transpose(L @ second_orderB) @ Psi_X[:, l].reshape(-1, 1)) - ((np.transpose(second_orderB) @ nablaPsi[:, :, l]) @ b_v2(l))
+# the a function
+# this was calculated in a weird way, so could have issues...
+L_times_second_orderB_transpose = (L @ second_orderB).T
+# l = 1
+# print("1:", L.shape)
+# print("2:", second_orderB.shape)
+# print("3:", L_times_second_orderB_transpose.shape)
+# print("4:", Psi_X[:, l].shape) #.reshape(-1, 1)
+# print("5:", (L_times_second_orderB_transpose @ Psi_X[:, l]).shape)
+# print("6:", second_orderB.T.shape)
+# print("7:", nablaPsi[:, :, l].shape)
+# print("8:", b_v2(l).shape) # (20,)
+# print("9:", (second_orderB.T @ nablaPsi[:, :, l]).shape)
+# print("10:", (second_orderB.T @ nablaPsi[:, :, l] @ b_v2(l)).shape)
+def a(l):
+    return (L_times_second_orderB_transpose @ Psi_X[:, l]) - \
+        (second_orderB.T @ nablaPsi[:, :, l] @ b_v2(l))
 
-# x_str = ""
-# for i in range(d):
-#     x_str += 'x_' + str(i) + ', '
-# x_syms = symbols(x_str)
-# M = itermonomials(x_syms, 4)
-# sortedM = sorted(M, key=monomial_key('grlex', np.flip(x_syms)))
+a_1 = a(1)
+print(a_1.shape)
+print(a(1).reshape((d,d)))
 
-# # Function to compute the a matrix at a specific snapshot index
+## Function to compute the a matrix at a specific snapshot index
 # def evalAMatrix(l):
 #     a_matrix = np.zeros((d, d))
 #     for p in range(d+1, d+1+s):
@@ -186,3 +200,4 @@ print(count) # 97
 
 # # np.save('gedmd_epsilons_fixed', epsilons)
 # # np.savetxt("gedmd_epsilons_fixed.csv", epsilons.reshape(-1, epsilons.shape[1]), delimiter=",")
+# %%
---
+++ scrap.py
@@ -0,0 +1,5 @@
+#%% Misc Array Testing
+import numpy as np
+a = np.array([1,2,3])
+
+b = 1 + 2j
