5ddf986a3bd8d41c53f14c65749fe9b0a392e60d
Preston Rozwood <279pdbz199@gmail.com>
2021-04-27 00:28:35 -0700

    Attempting to learn autograd

--- gradient.py
+++ gradient.py
@@ -1,12 +1,24 @@
-import numpy as np
+#%%
+import observables
+import tensorflow as tf
+physical_devices = tf.config.list_physical_devices('GPU') 
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-B = np.array([
-    [1, 1],
-    [1, 1]
-])
+B = tf.Variable(tf.ones((3,2)))
 
 # Psi_X = dictionaries applied to x
+# nabla_X.T @ (B.T @ Psi_X)
+# monomials(2) = 1, x_1, x_2, x_1**2, x_1*x_2, x_2**2
 
-# nabla_X.T @ B.T @ Psi_X
+#%%
+psi = observables.monomials(2)
+Psi_X = psi(tf.constant([[2,2]]))
 
-# May not be immediately useful
+# %%
+with tf.GradientTape(persistent=True) as tape:
+    y = tf.transpose(B) @ Psi_X
+
+# Not really sure about correctness...
+grad = tape.gradient(y, B)
+
+# %%
