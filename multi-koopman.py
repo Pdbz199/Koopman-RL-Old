f1562d5179c0ab5cdf06e70898349c8ceda9090a
Preston Rozwood <279pdbz199@gmail.com>
2021-05-16 14:19:52 -0700

    Datasets fixed

--- multi-koopman.py
+++ multi-koopman.py
@@ -3,37 +3,62 @@ import algorithms
 import numpy as np
 
 #%% Load data
-X = np.load('optimal-agent/cartpole-states.npy').T
-U = np.load('optimal-agent/cartpole-actions.npy').reshape(1,-1)
-X_0 = []
-Y_0 = []
-X_1 = []
-Y_1 = []
-for i in range(U.shape[1]):
-    action = U[0,i]
-    if action == 0 and i != U.shape[1]-1:
-        X_0.append(X[:,i])
-        Y_0.append(X[:,i+1])
-    elif action == 1 and i != U.shape[1]-1:
-        X_1.append(X[:,i])
-        Y_1.append(X[:,i+1])
+# X = np.load('optimal-agent/cartpole-states.npy').T
+# U = np.load('optimal-agent/cartpole-actions.npy').reshape(1,-1)
+# X_0 = []
+# Y_0 = []
+# X_1 = []
+# Y_1 = []
+# for i in range(U.shape[1]):
+#     action = U[0,i]
+#     if action == 0 and i != U.shape[1]-1:
+#         X_0.append(X[:,i])
+#         Y_0.append(X[:,i+1])
+#     elif action == 1 and i != U.shape[1]-1:
+#         X_1.append(X[:,i])
+#         Y_1.append(X[:,i+1])
 
-X_0 = np.array(X_0).T
-Y_0 = np.array(Y_0).T
-X_1 = np.array(X_1).T
-Y_1 = np.array(Y_1).T
+# X_0 = np.array(X_0).T
+# Y_0 = np.array(Y_0).T
+# X_1 = np.array(X_1).T
+# Y_1 = np.array(Y_1).T
 
-train_ind = int(np.around(X.shape[1] * 0.80))
-X_0_train = X_0[:,:train_ind]
-Y_0_train = Y_0[:,:train_ind]
-X_1_train = X_1[:,:train_ind]
-Y_1_train = Y_1[:,:train_ind]
+X = np.load('random-agent/cartpole-states.npy').T
+state_dim = X.shape[0]
+X_0 = np.load('random-agent/cartpole-states-0.npy').T
+Y_0 = np.load('random-agent/cartpole-next-states-0.npy').T
+X_1 = np.load('random-agent/cartpole-states-1.npy').T
+Y_1 = np.load('random-agent/cartpole-next-states-1.npy').T
+
+U = np.load('random-agent/cartpole-actions.npy').reshape(1,-1)
+
+percent_training = 0.8
+train_inds = [
+    int(np.around(X_0.shape[1]*percent_training)),
+    int(np.around(X_1.shape[1]*percent_training))
+]
+X_0_train = X_0[:,:train_inds[0]]
+Y_0_train = Y_0[:,:train_inds[0]]
+X_1_train = X_1[:,:train_inds[1]]
+Y_1_train = Y_1[:,:train_inds[1]]
+
+#%% Median trick
+num_pairs = 1000
+pairwise_distances = []
+for _ in range(num_pairs):
+    i, j = np.random.choice(np.arange(X.shape[1]), 2)
+    x_i = X[:,i]
+    x_j = X[:,j]
+    pairwise_distances.append(np.linalg.norm(x_i - x_j))
+pairwise_distances = np.array(pairwise_distances)
+gamma = np.quantile(pairwise_distances, 0.9)
 
 #%% RBF Sampler
 from sklearn.kernel_approximation import RBFSampler
-rbf_feature = RBFSampler(gamma=0.7, random_state=1)
+rbf_feature = RBFSampler(gamma=gamma, random_state=1)
 X_features = rbf_feature.fit_transform(X)
-psi = lambda x: X_features.T @ x.reshape(-1,1)
+def psi(x):
+    return X_features.T @ x.reshape((state_dim,1))
 # X_0_features = rbf_feature.fit_transform(X_0)
 # X_1_features = rbf_feature.fit_transform(X_1)
 # k_0 = X_0_features.shape[1]
@@ -73,38 +98,56 @@ K_1 = algorithms.rrr(Psi_X_1.T, Psi_Y_1.T).T
 B_0 = algorithms.SINDy(Psi_X_0.T, X_0_train.T, X_0_train.shape[0])
 B_1 = algorithms.SINDy(Psi_X_1.T, X_1_train.T, X_1_train.shape[0])
 
-#%%
+#%% Prediction compounding error
 import gym
 env = gym.make('CartPole-v0')
 horizon = 1000
-# data_point_index = 6000 + int(np.around((np.random.rand() * X.shape[1] - 6000)))
-# print(data_point_index)
-num_trials = 1000
+num_trials = 1#000
 norms = []
 for i in range(num_trials):
-    # action_path = U[0, data_point_index:data_point_index+horizon]
     action_path = [np.random.choice([0,1]) for i in range(horizon)]
     trial_norms = []
     true_state = env.reset()
     predicted_state = true_state.copy()
     for h in range(horizon):
         action = action_path[h]
-        psi_x = psi(predicted_state.reshape(-1,1))
-        if action == 0:
-            predicted_state = B_0.T @ K_0 @ psi_x
-        else:
-            predicted_state = B_1.T @ K_1 @ psi_x
+        psi_x = psi(predicted_state)
+        predicted_state = B_0.T @ K_0 @ psi_x if action == 0 else B_1.T @ K_1 @ psi_x
         true_state, ___, __, _ = env.step(action)
-        
-        norm = np.sum( np.power( (true_state - predicted_state) , 2 ) )
+
+        norm = np.sum( np.power( ( true_state.reshape(-1,1) - predicted_state ) , 2 ) )
         trial_norms.append(norm)
     norms.append(trial_norms)
 
-# %%
+#%%
 import matplotlib.pyplot as plt
 plt.plot(np.mean(norms, axis=0))
-plt.ylabel('Difference between norms')
+plt.ylabel('L2 Norm')
 plt.xlabel('Timestep')
 plt.show()
 
-# %%
+#%% One-step prediction error
+# data_point_index = 1000
+horizon = 1000
+norms = []
+# action_path = U[0, data_point_index:data_point_index+horizon]
+action_path = U[0, :horizon]
+norms = []
+starting_point = int(np.around(np.random.rand() * X.shape[1]))
+true_state = X[:,starting_point]
+for h in range(horizon):
+    action = action_path[h]
+    psi_x = psi(true_state)
+    predicted_state = B_0.T @ K_0 @ psi_x if action == 0 else B_1.T @ K_1 @ psi_x
+    true_state = X[:,starting_point+h+1]
+
+    norm = np.sum( np.power( ( true_state.reshape(-1,1) - predicted_state ) , 2 ) )
+    norms.append(norm)
+
+#%%
+plt.plot(norms, marker='.', linestyle='')
+plt.ylabel('L2 Norm')
+plt.xlabel('Timestep')
+plt.show()
+
+#%%
--- randomRL.py
+++ randomRL.py
@@ -10,11 +10,14 @@ episodes = 200
 steps_per_episode = 100
 
 states = []
+states_0 = []
+states_1 = []
+next_states_0 = []
+next_states_1 = []
 actions = []
 rewards = []
 
 #%%
-current_state = env.reset()
 for episode in range(episodes):
     observation = env.reset()
     for t in range(steps_per_episode):
@@ -22,24 +25,38 @@ for episode in range(episodes):
 
         action = env.action_space.sample()
 
-        states.append(current_state)
+        if action == 0:
+            states_0.append(observation)
+        else:
+            states_1.append(observation)
+        states.append(observation)
         actions.append(action)
 
-        new_state, reward, done, _ = env.step(action) # take a random action
-        rewards.append(reward)
+        observation, reward, done, _ = env.step(action) # take a random action
 
-        current_state = new_state
+        if action == 0:
+            next_states_0.append(observation)
+        else:
+            next_states_1.append(observation)
+
+        rewards.append(reward)
 env.close()
 
 states = np.array(states)
+states_0 = np.array(states_0)
+states_1 = np.array(states_1)
 actions = np.array(actions)
 rewards = np.array(rewards)
 
-np.save('random-cartpole-states', states)
-np.save('random-cartpole-actions', actions)
-np.save('random-cartpole-rewards', rewards)
+np.save('random-agent/cartpole-states', states)
+np.save('random-agent/cartpole-states-0', states_0)
+np.save('random-agent/cartpole-states-1', states_1)
+np.save('random-agent/cartpole-next-states-0', next_states_0)
+np.save('random-agent/cartpole-next-states-1', next_states_1)
+np.save('random-agent/cartpole-actions', actions)
+np.save('random-agent/cartpole-rewards', rewards)
 
 #%%
 print(rewards.shape)
-print(np.sum(rewards))
+print(np.mean(rewards))
 # %%
