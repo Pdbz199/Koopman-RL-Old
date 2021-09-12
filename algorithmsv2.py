8e28a74b81d682d0a52cbb09f8152cfe3cb286e6
Preston Rozwood <279pdbz199@gmail.com>
2021-09-12 12:30:21 -0700

    Some control work

--- algorithmsv2.py
+++ algorithmsv2.py
@@ -1,7 +1,8 @@
-import numpy as np
 import mpmath as mp
+import numpy as np
+import scipy.integrate as integrate
 
-# you can pass functions as parameters to jitted functions IF AND ONLY IF they re also jitted
+# you can pass functions as parameters to jitted functions IF AND ONLY IF they're also jitted
 
 def rho(u, o='unif', a=0 ,b=1):
         if o == 'unif':
@@ -14,28 +15,34 @@ def K_u(K, psi_u):
     return np.einsum('ijz,z->ij', K, psi_u)
 
 def algorithm2(X, U, phi, psi, K_hat, cost, learning_rate=0.1, epsilon=1):
+    # get dimension of states and actions from tensor
     num_lifted_state_features = K_hat.shape[0]
     num_lifted_action_features = K_hat.shape[2]
 
-    w = np.empty(num_lifted_state_features)
-    w.fill(1)
+    # start with weight vector of all ones
+    w = np.ones(num_lifted_state_features)
+    
+    # unnormalized optimal policy
+    def pi(u, x, u_upper, u_lower):
+       pi_u = mp.exp(-1 * (cost(x, u) + w @ K_u(K_hat, psi(u)) @ phi(x))) # / Z_x
+    
+    Z_x = integrate(pi, a, b, args)
+    output = pi(args)
+    output / Z_x
 
-    def pi(u, x):
-        return mp.exp(-1 * (cost(x, u) + w @ K_u(K_hat, psi(u)) @ phi(x)))
-
-    x1 = X[:, int(np.random.uniform(0, X.shape[1]))]
-    def bellmanError(w, pi):
+    def bellmanError(w, pi, phi_x):
         total = 0
         for i in range(X.shape[1]):
-            phi_x = phi(x1)
             expectation_u = 0
-            for u in U: # U is a collection of ALL possible actions as row vectors
+            for u in U: # U is a collection of all POSSIBLE actions as row vectors
                 u = u[0]
                 expectation_u += ( cost(x1, u) - mp.log( pi(u, x1) ) - w @ K_u(K_hat, psi(u)) @ phi_x ) * pi(u, x1)
             total += np.power(( w @ phi_x - expectation_u ), 2)
         return total
 
-    BE = bellmanError(w, pi)
+    # sample initial state x1 from sample of snapshots
+    x1 = X[:, int(np.random.uniform(0, X.shape[1]))]
+    BE = bellmanError(w, pi, phi(x1))
     while BE > epsilon:
         # These are row vectors
         u1 = U[0, int(np.random.uniform(0, num_lifted_action_features))]
@@ -49,7 +56,7 @@ def algorithm2(X, U, phi, psi, K_hat, cost, learning_rate=0.1, epsilon=1):
 
         w = w - (learning_rate * nabla_w)
 
-        BE = bellmanError(w, pi)
+        BE = bellmanError(w, pi, phi_x1)
         print(BE) # not decreasing at all (value is around 19000)
 
     return pi
---
+++ koopman-tensor/optimal-control/slow-manifold.py
@@ -0,0 +1,196 @@
+#%% Imports
+import importlib
+import gym
+import numpy as np
+import numba as nb
+import matplotlib.pyplot as plt
+import scipy as sp
+import sys
+sys.path.append("../../")
+import estimate_L
+import auxiliaries
+import algorithmsv2
+
+from control import lqr
+from scipy import integrate
+from sklearn.kernel_approximation import RBFSampler
+
+#%% System variable definitions
+mu = -0.1
+lamb = -0.5
+
+A = np.array([
+    [mu, 0   ],
+    [0,  lamb]
+])
+B = np.array([
+    [0],
+    [1]
+])
+Q = np.identity(2)
+R = 1
+
+K = np.array(
+    [[mu, 0,    0    ],
+     [0,  lamb, -lamb],
+     [0,  0,    2*mu ]]
+)
+B2 = np.array(
+    [[0],
+     [1],
+     [0]]
+)
+Q2 = np.array(
+    [[1, 0, 0],
+     [0, 1, 0],
+     [0, 0, 0]]
+)
+
+#%%
+x = np.array([
+    [-5.],
+    [5.]
+])
+y = np.append(x, [x[0]**2], axis=0)
+
+#%% Standard LQR
+C = lqr(A, B, Q, R)[0][0]
+print("Standard LQR:", C)
+# C = np.array([0,2.4142]) when lamb = 1
+
+#%% Koopman LQR
+# C = [0.0, 0.61803399, 0.23445298] when lamb = -0.5
+C2 = lqr(K, B2, Q2, R)[0][0]
+print("Koopman LQR:", C2)
+# C = np.array([0.0, 2.4142, -1.4956])
+# u = ((-C[:2] @ x) - (C[2] * x[0]**2))[0]
+
+def vf(tau, x, u):
+    returnVal = ((A @ x.reshape(-1,1)) + np.array([[0], [-lamb * x[0]**2]]) + (B @ u.reshape(-1,1)))
+    return returnVal[:,0]
+
+def C2_func(x):
+    np.random.seed( np.abs( int( hash(str(x)) / (10**10) ) ) )
+    return np.array([
+        np.random.uniform(-100,100),
+        np.random.uniform(-100,100),
+        np.random.uniform(-100,100)
+    ])
+
+def U_builder(X):
+    U = []
+    for x in X.T:
+        U.append([-C2_func(x)@x])
+    return np.array(U).T
+
+#%% Randomly controlled system (currently takes 5-ever to run
+# X = integrate.solve_ivp(lambda tau, x: vf(tau, x, ((-C2_func(x)[:2] @ x) - (C2_func(x)[2] * x[0]**2))), (0,50), x[:,0], first_step=0.05, max_step=0.05)
+# X = X.y
+# Y = np.roll(X, -1, axis=1)[:,:-1]
+# X = X[:,:-1]
+# U = U_builder(X)
+
+#%% Standard LQR controlled system
+X_opt = integrate.solve_ivp(lambda tau, x: vf(tau, x, ((-C2[:2] @ x) - (C2[2] * x[0]**2))), (0,50), x[:,0], first_step=0.05, max_step=0.05)
+X_opt = X_opt.y
+Y_opt = np.roll(X_opt, -1, axis=1)[:,:-1]
+X_opt = X_opt[:,:-1]
+U_opt = (-C@X_opt).reshape(1,-1)
+
+#%% Koopman LQR controlled system
+# X2 = integrate.solve_ivp(lambda tau, x: vf(tau, x, ((-C2[:2] @ x) - (C2[2] * x[0]**2))), (0,50), x[:,0], first_step=0.05, max_step=0.05)
+# X2 = X2.y[:,:-2]
+# U2 = getKoopmanAction(X2).reshape(1,-1)
+# Y2 = np.apply_along_axis(lambda x: np.append(x, [x[0]**2]), axis=0, arr=X)
+
+def cost(x, u):
+    return (x @ Q @ x + u * R * u)
+
+#%% Matrix builder functions
+def phi(x):
+    return np.array([1, x[0], x[1], x[0]**2, x[1]**2, x[0]*x[1]])
+
+def psi(u):
+    return np.array([float(1), float(u), float(u**2)])
+
+def getPhiMatrix(X):
+    Phi_X = []
+    for x in X.T:
+        Phi_X.append(phi(x))
+
+    return np.array(Phi_X).T
+
+def getPsiMatrix(U):
+    Psi_U = []
+    for u in U.T:
+        Psi_U.append(psi(u))
+
+    return np.array(Psi_U).T
+
+#%%
+Phi_X = getPhiMatrix(X_opt)
+Psi_U = getPsiMatrix(U_opt)
+print("Phi_X shape:", Phi_X.shape)
+print("Psi_U shape:", Psi_U.shape)
+
+num_lifted_state_observations = Phi_X.shape[1]
+num_lifted_state_features = Phi_X.shape[0]
+num_lifted_action_observations = Psi_U.shape[1]
+num_lifted_action_features = Psi_U.shape[0]
+
+# @nb.njit(fastmath=True)
+def getPsiPhiMatrix(Psi_U, Phi_X):
+    psiPhiMatrix = np.empty((num_lifted_action_features * num_lifted_state_features, num_lifted_state_observations))
+
+    for i in range(num_lifted_state_observations):
+        kron = np.kron(Psi_U[:,i], Phi_X[:,i])
+        psiPhiMatrix[:,i] = kron
+
+    return psiPhiMatrix
+
+#%%
+psiPhiMatrix = getPsiPhiMatrix(Psi_U, Phi_X)
+print("PsiPhiMatrix shape:", psiPhiMatrix.shape)
+M = estimate_L.ols(psiPhiMatrix.T, getPhiMatrix(Y_opt).T).T
+print("M shape:", M.shape)
+assert M.shape == (num_lifted_state_features, num_lifted_state_features * num_lifted_action_features)
+
+K = np.empty((num_lifted_state_features, num_lifted_state_features, num_lifted_action_features))
+for i in range(M.shape[0]):
+    K[i] = M[i].reshape((num_lifted_state_features, num_lifted_action_features), order='F')
+print("K shape:", K.shape)
+
+def K_u(u):
+    return np.einsum('ijz,z->ij', K, psi(u))
+
+print("Psi U[0,0]:", psi(U_opt[0,0]))
+print("K_u shape:", K_u(U_opt[0,0]).shape)
+
+#%%
+def l2_norm(true_state, predicted_state):
+    return np.sum( np.power( ( true_state - predicted_state ), 2 ) )
+
+#%% Training error
+norms = []
+starting_point = 0
+for i in range(Y_opt.shape[1]):
+    actual_phi_x_prime = phi(Y_opt[:,starting_point+i])
+    predicted_phi_x_prime = K_u(U_opt[0,starting_point+i]) @ phi(X_opt[:,starting_point+i])
+
+    norms.append(l2_norm(actual_phi_x_prime, predicted_phi_x_prime))
+norms = np.array(norms)
+# print(norms)
+print("Mean training norm:", norms.mean())
+
+#%% Single-step prediction error with optimal controller
+norms = []
+starting_point = 0
+for i in range(Y_opt.shape[1]):
+    actual_phi_x_prime = phi(Y_opt[:,starting_point+i])
+    predicted_phi_x_prime = K_u(U_opt[0,starting_point+i]) @ phi(X_opt[:,starting_point+i])
+
+    norms.append(l2_norm(actual_phi_x_prime, predicted_phi_x_prime))
+norms = np.array(norms)
+print("Mean single-step prediction norm:", norms.mean())
+
+#%%
