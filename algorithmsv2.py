Changes for more correct error computation

algorithmsv2.py
@@ -86,7 +86,7 @@ class algos:
             # x = self.X[:, np.random.choice(np.arange(self.X.shape[1]))].reshape(-1,1)
             phi_x = self.phi(x)
 
-            inner_pi_us = self.inner_pi_us(self.All_U, x) #vectorize
+            inner_pi_us = self.inner_pi_us(self.All_U, x)
             inner_pi_us = np.real(inner_pi_us)
             max_inner_pi_u = np.max(inner_pi_us)
             pi_us = np.exp(inner_pi_us - max_inner_pi_u)
@@ -110,9 +110,9 @@ class algos:
         total = 0
         self.All_U = np.random.uniform(self.u_lower, self.u_upper, [1,self.u_batch_size])
         # print(self.All_U.shape)
-        for _ in range(int(self.X.shape[1]/100)): # loop
-            # x = self.X[:,i].reshape(-1,1)
-            x = self.X[:, np.random.choice(np.arange(self.X.shape[1]))].reshape(-1,1)
+        for i in range(0, self.X.shape[1]): # loop
+            x = self.X[:,i].reshape(-1,1)
+            # x = self.X[:, np.random.choice(np.arange(self.X.shape[1]))].reshape(-1,1)
             phi_x = self.phi(x)
 
             inner_pi_us = self.inner_pi_us(self.All_U, x)

koopman-tensor/optimal-control/double_well_control_1D.py
@@ -140,23 +140,11 @@ def cost(x, u):
     return (x[0]**2) #Tried to normalize this by 10000 for overflow issues, but didn't help
 
 #%% Discretize all controls
-def discretize(start, end, num_points):
-    step_size = (np.abs(start) + np.abs(end)) / num_points
-    ret = [start]
-    for i in range(1,num_points):
-        ret.append(ret[i-1] + step_size)
-    return ret
-
-U = []
-for i in range(41):
-    U.append([-2 + (i * 0.1)])
-U = np.array(U)
+step_size = 0.1
+All_U = np.arange(start=u_bounds[0,0], stop=u_bounds[0,1]+step_size, step=0.1).reshape(-1,1)
 
 #%% Control
-algos = algorithmsv2.algos(X, U, u_bounds[0], phi, psi, K, cost, epsilon=1e-2, bellmanErrorType=1, u_batch_size=20)
+algos = algorithmsv2.algos(X, All_U, u_bounds[0], phi, psi, K, cost, epsilon=1e-2, bellmanErrorType=1, u_batch_size=20)
 bellmanErrors, gradientNorms = algos.algorithm2(batch_size=64)
-# pi = algos.algorithm3()
 
-#%% Bellman Errors
-# 1184.3180405984
-# 3508912268.71883
+#%%