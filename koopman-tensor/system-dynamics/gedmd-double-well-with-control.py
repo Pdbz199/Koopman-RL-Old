864edd51bd573a7e2f536a1d7615c1f6c8cb1986
Preston Rozwood <279pdbz199@gmail.com>
2021-12-15 14:09:42 -0800

    Removed randomness from eigs + added slider for eigenfunctions

--- koopman-tensor/system-dynamics/gedmd-double-well-with-control.py
+++ koopman-tensor/system-dynamics/gedmd-double-well-with-control.py
@@ -1,7 +1,10 @@
 #%% Imports
 import numpy as np
+np.random.seed(123)
 import matplotlib
 import matplotlib.pyplot as plt
+
+from matplotlib.widgets import Slider
 
 import sys
 sys.path.append('../../')
@@ -77,20 +80,59 @@ for i in range(dim_phi):
 def K_u(K, u):
     return np.einsum('ijz,zk->ij', K, psi(u))
 
-#%% Plot eigenfunctions
-evs = 3
-_, _V = utilities.sortEig(K_u(K, np.array([[0]])).T, evs, which='SM')
-
+#%% Compute values for plotting eigenfunctions
 fig = plt.figure()
 c = x_omega.midpointGrid()
 _X = c[0, :].reshape(x_omega._boxes)
 _Y = c[1, :].reshape(x_omega._boxes)
 Phi_c = phi(c)
 
+evs = 3
+
+axes = []
 for i in range(evs):
-    ax = fig.add_subplot(1, 3, i+1, projection='3d')
-    _Z = np.real( _V[:, i].T @ Phi_c ).reshape(x_omega._boxes)
-    ax.plot_surface(_X, _Y, _Z, cmap=matplotlib.cm.coolwarm)
+    axes.append(fig.add_subplot(1, evs+1, i+1+1, projection='3d'))
 
 #%%
-plt.show()
+# fig = plt.figure()
+# _, _V = utilities.sortEig(K_u(K, np.array([[0]])).T, evs, which='SM')
+# for i in range(evs):
+#     ax = fig.add_subplot(1, 3, i+1, projection='3d')
+#     _Z = np.real( _V[:, i].T @ Phi_c ).reshape(x_omega._boxes)
+#     ax.plot_surface(_X, _Y, _Z, cmap=matplotlib.cm.coolwarm)
+
+#%% Define initial parameters
+axcolor = 'lightgoldenrodyellow'
+
+init_c = 0
+
+#%%
+ax_c = plt.axes([0.15, 0.25, 0.0225, 0.63], facecolor=axcolor)
+c_slider = Slider(
+    ax=ax_c,
+    label="c",
+    valmin=-2,
+    valmax=2,
+    valinit=init_c,
+    orientation="vertical"
+)
+
+#%%
+# The function to be called anytime a slider's value changes
+def update(val):
+    _, _V = utilities.sortEig(K_u(K, np.array([[c_slider.val]])).T, evs, which='SM')
+
+    for i in range(evs):
+        axes[i].clear()
+        _Z = np.real( _V[:, i].T @ Phi_c ).reshape(x_omega._boxes)
+        axes[i].plot_surface(_X, _Y, _Z, cmap=matplotlib.cm.coolwarm)
+
+#%% Register the update function with each slider
+c_slider.on_changed(update)
+
+#%%
+while(True):
+    update(0)
+    plt.pause(0.01)
+
+#%%
