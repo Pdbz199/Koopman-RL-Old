c41495cba58bc87071a3619b334b11d555fc08d2
Preston Rozwood <279pdbz199@gmail.com>
2022-04-28 00:26:07 -0700

    Works

--- koopman-tensor/optimal-control/cartpole-torch.py
+++ koopman-tensor/optimal-control/cartpole-torch.py
@@ -2,13 +2,18 @@ import gym
 import numpy as np
 import torch
 
+seed = 123
+np.random.seed(seed)
+torch.manual_seed(seed)
+
 from matplotlib import pyplot as plt
+from scipy.special import comb
 
 import sys
 sys.path.append('../')
 from tensor import KoopmanTensor
 sys.path.append('../../')
-import cartpole_reward
+# import cartpole_reward
 import observables
 
 env = gym.make('CartPole-v0')
@@ -60,51 +65,61 @@ while i < N:
 X = X[:,:-1]
 
 #%% Estimate Koopman tensor
+order = 2
 tensor = KoopmanTensor(
     X,
     Y,
     U,
-    phi=observables.monomials(2),
-    psi=observables.monomials(2),
+    phi=observables.monomials(order),
+    psi=observables.monomials(order),
     regressor='ols'
 )
 
-obs_size = env.observation_space.shape[0]
+# obs_size = env.observation_space.shape[0]
+state_dim = env.observation_space.shape[0]
+Ns = np.arange( state_dim - 1, state_dim - 1 + (order+1) )
+obs_size = int( np.sum( comb( Ns, np.ones_like(Ns) * (order+1) ) ) )
 n_actions = env.action_space.n
-HIDDEN_SIZE = 256
+# HIDDEN_SIZE = 256
 
+# model = torch.nn.Sequential(
+#     torch.nn.Linear(obs_size, HIDDEN_SIZE),
+#     torch.nn.ReLU(),
+#     torch.nn.Linear(HIDDEN_SIZE, n_actions),
+#     torch.nn.Softmax(dim=0)
+# )
 model = torch.nn.Sequential(
-    torch.nn.Linear(obs_size, HIDDEN_SIZE),
-    torch.nn.ReLU(),
-    torch.nn.Linear(HIDDEN_SIZE, n_actions),
+    torch.nn.Linear(obs_size, n_actions),
     torch.nn.Softmax(dim=0)
 )
 print("Model:", model)
 
+# learning_rate = 0.003
 learning_rate = 0.003
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 Horizon = 500
-MAX_TRAJECTORIES = 750
+MAX_TRAJECTORIES = 5000 # 500, 750
 gamma = 0.99
 score = []
 
 for trajectory in range(MAX_TRAJECTORIES):
-    curr_state = env.reset()
+    curr_state = np.vstack(env.reset()) # column vector
     done = False
     transitions = []
 
     for t in range(Horizon):
-        act_prob = model(torch.from_numpy(curr_state).float())
+        phi_curr_state = tensor.phi(curr_state) # column vector
+        act_prob = model(torch.from_numpy(phi_curr_state[:,0]).float())
         action = np.random.choice(np.array([0,1]), p=act_prob.data.numpy())
-        prev_state = curr_state
+        prev_state = curr_state[:,0]
         # curr_state, _, done, info = env.step(action)
-        curr_state = (tensor.B.T @ tensor.K_(np.array([[action]])) @ tensor.phi(np.vstack(curr_state)))[:,0]
+        curr_state = tensor.B.T @ tensor.K_(np.array([[action]])) @ phi_curr_state
         done = bool(
-            curr_state[0] < -env.x_threshold
-            or curr_state[0] > env.x_threshold
-            or curr_state[2] < -env.theta_threshold_radians
-            or curr_state[2] > env.theta_threshold_radians
+            curr_state[0,0] < -env.x_threshold
+            or curr_state[0,0] > env.x_threshold
+            or curr_state[2,0] < -env.theta_threshold_radians
+            or curr_state[2,0] > env.theta_threshold_radians
         )
         transitions.append((prev_state, action, t+1))
         if done:
@@ -123,7 +138,7 @@ for trajectory in range(MAX_TRAJECTORIES):
     expected_returns_batch = torch.FloatTensor(batch_Gvals)
     expected_returns_batch /= expected_returns_batch.max()
 
-    state_batch = torch.Tensor(np.array([s for (s,a,r) in transitions])) #* Warning that this is slow
+    state_batch = torch.Tensor(tensor.phi(np.array([s for (s,a,r) in transitions]).T).T)
     action_batch = torch.Tensor([a for (s,a,r) in transitions])
 
     pred_batch = model(state_batch)
@@ -158,22 +173,24 @@ plt.plot(avg_score, color='blue', linewidth=3)
 plt.scatter(np.arange(score.shape[0]), score, color='green' , linewidth=0.3)
 # plt.show()
 
-num_episodes = 100
+num_episodes = 5
 def watch_agent():
     rewards = np.zeros([num_episodes])
     for episode in range(num_episodes):
-        state = env.reset()
+        state = np.vstack(env.reset())
         done = False
         episode_rewards = []
         while not done:
-            # env.render()
-            pred = model(torch.from_numpy(state).float())
+            env.render()
+            phi_state = tensor.phi(state)
+            pred = model(torch.from_numpy(phi_state[:,0]).float())
             action = np.random.choice(np.array([0,1]), p=pred.data.numpy())
             state, reward, done, _ = env.step(action)
+            state = np.vstack(state)
             episode_rewards.append(reward)
             if done:
                 rewards[episode] = np.sum(episode_rewards)
                 print("Reward:", rewards[episode])
-    # env.close()
+    env.close()
     print(f"Mean reward per episode over {num_episodes} episodes:", np.mean(rewards))
 watch_agent()
