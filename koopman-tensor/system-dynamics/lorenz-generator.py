4ea93c197828530e4f6e63cf1e1136a7f52c665b
Preston Rozwood <279pdbz199@gmail.com>
2022-06-22 01:28:29 -0700

    Learning Lorenz dynamics

--- koopman-tensor/system-dynamics/lorenz-generator.py
+++ koopman-tensor/system-dynamics/lorenz-generator.py
@@ -11,7 +11,7 @@ from scipy.special import comb
 
 import sys
 sys.path.append('../')
-from tensor import KoopmanTensor
+from tensor import KoopmanTensor, SINDy
 sys.path.append('../../')
 import observables
 
@@ -24,8 +24,11 @@ action_order = 2
 
 state_column_shape = [state_dim, 1]
 action_column_shape = [action_dim, 1]
+
 phi_dim = int( comb( state_order+state_dim, state_order ) )
 psi_dim = int( comb( action_order+action_dim, action_order ) )
+
+phi_column_shape = [phi_dim, 1]
 
 action_range = np.array([-50, 50])
 # action_range = np.array([-75, 75])
@@ -36,7 +39,9 @@ all_actions = np.round(all_actions, decimals=2)
 sigma = 10
 rho = 28
 beta = 8/3
-t_span = np.arange(0, 0.0001, 0.00001)
+
+dt = 0.001
+t_span = np.arange(0, dt, dt/10)
 
 x_e = np.sqrt( beta * ( rho - 1 ) )
 y_e = np.sqrt( beta * ( rho - 1 ) )
@@ -56,9 +61,9 @@ def continuous_f(action=None):
         """
         x, y, z = input
 
-        x_dot = sigma * ( y - x )
-        y_dot = ( rho - z ) * x - y
-        z_dot = x * y - beta * z
+        x_dot = sigma * ( y - x )   # sigma*y - sigma*x
+        y_dot = ( rho - z ) * x - y # rho*x - x*z - y
+        z_dot = x * y - beta * z    # x*y - beta*z
 
         u = action
         if u is None:
@@ -84,8 +89,8 @@ def f(state, action):
     return np.vstack(soln.y[:,-1])
 
 #%% Generate data
-num_episodes = 100
-num_steps_per_episode = 10000
+num_episodes = 500
+num_steps_per_episode = 1000
 N = num_episodes*num_steps_per_episode # Number of datapoints
 X = np.zeros([state_dim,N])
 Y = np.zeros([state_dim,N])
@@ -120,6 +125,84 @@ tensor = KoopmanTensor(
     regressor='ols'
 )
 
+Psi_U = tensor.psi(U)
+Phi_X = tensor.phi(X)
+
+# Build matrix of kronecker products between u_i and x_i for all 0 <= i <= N
+kronMatrix = np.empty([
+    psi_dim * phi_dim,
+    N
+])
+for i in range(N):
+    kronMatrix[:,i] = np.kron(
+        Psi_U[:,i],
+        Phi_X[:,i]
+    )
+
+dX = (Y - X) / dt
+# dPhi_Y = np.einsum('ijk,jk->ik', tensor.phi.diff(X), dX)
+# regression_Y = dPhi_Y
+M = SINDy(kronMatrix.T, dX.T).T
+
+# reshape M into tensor K
+K = np.empty([
+    state_dim,
+    phi_dim,
+    psi_dim
+])
+for i in range(state_dim):
+    K[i] = M[i].reshape(
+        [phi_dim, psi_dim],
+        order='F'
+    )
+
+def K_(u):
+    ''' Pick out Koopman operator given an action '''
+
+    # If array, convert to column vector
+    if isinstance(u, int) or isinstance(u, float) or isinstance(u, np.int64) or isinstance(u, np.float64):
+        u = np.array([[u]])
+    elif len(u.shape) == 1:
+        u = np.vstack(u)
+    
+    K_u = np.einsum('ijz,zk->kij', K, tensor.psi(u))
+
+    if K_u.shape[0] == 1:
+        return K_u[0]
+
+    return K_u
+
+#%% Continuous dynamics with Koopman
+def continuous_tensor_f(action=None):
+    """
+        INPUTS:
+        action - action vector. If left as None, then random policy is used
+    """
+
+    def f_u(t, input):
+        """
+            INPUTS:
+            input - state vector
+            t - timestep
+        """
+        u = action
+        if u is None:
+            u = np.random.choice(all_actions, size=action_column_shape)
+
+        x_column_vector = np.zeros(state_column_shape)
+        for i in range(state_dim):
+            x_column_vector[i,0] = input[i]
+
+        x_dot_column_vector = K_(u) @ tensor.phi(x_column_vector)
+
+        x_dot = np.zeros(state_dim)
+        for i in range(state_dim):
+            x_dot[i] = x_dot_column_vector[i,0] + (u if i == 0 else 0)
+
+        return x_dot
+
+    return f_u
+
 #%% Policy function(s)
 def zero_policy(x):
     return np.array([[0]])
@@ -128,18 +211,12 @@ def random_policy(x):
     return np.random.choice(action_range, size=action_column_shape)
 
 #%% Compute true and learned dynamics over time
-initial_state = np.array([[0], [1], [1.05]])
-tspan = np.arange(0, 25, 0.001)
+initial_state = np.array([[2], [1], [1.05]])
+tspan = np.arange(0, 25, dt)
 u = zero_policy(initial_state)
 
 true_xs = odeint(continuous_f(u), initial_state[:,0], tspan, tfirst=True)
-learned_xs = np.zeros([25000,phi_dim])
-phi_state = tensor.phi(initial_state)
-for i in range(learned_xs.shape[0]):
-    # learned_xs[i] = (sp.linalg.expm((0.1*(i+1)) * tensor.K_(u)) @ phi_state)[:,0]
-    learned_xs[i] = (sp.linalg.expm(10.0 * tensor.K_(u)) @ phi_state)[:,0]
-    phi_state = np.vstack(learned_xs[i])
-learned_xs = (tensor.B.T @ learned_xs.T).T
+learned_xs = odeint(continuous_tensor_f(u), initial_state[:,0], tspan, tfirst=True)
 
 #%% Plot true vs. learned dynamics over time
 fig, axs = plt.subplots(2)
@@ -151,11 +228,53 @@ axs[0].set(xlabel='Step', ylabel='State value')
 axs[1].set_title('Learned dynamics')
 axs[1].set(xlabel='Step', ylabel='State value')
 
-labels = np.array(['x_0', 'x_1', 'x_2'])
+labels = np.array(['x_1', 'x_2', 'x_3'])
 for i in range(state_dim):
     axs[0].plot(true_xs[:,i], label=labels[i])
     axs[1].plot(learned_xs[:,i], label=labels[i])
 lines_labels = [axs[0].get_legend_handles_labels()]
+lines, labels = [sum(lol, []) for lol in zip(*lines_labels)]
+fig.legend(lines, labels)
+
+plt.tight_layout()
+plt.show()
+
+#%%
+fig = plt.figure()
+fig.suptitle('Dynamics Over Time')
+ax1 = fig.add_subplot(211, projection='3d')
+ax2 = fig.add_subplot(212, projection='3d')
+
+line1, = ax1.plot3D(
+    true_xs[:,0],
+    true_xs[:,1],
+    true_xs[:,2],
+    lw=0.75
+)
+line2, = ax2.plot3D(
+    learned_xs[:,0],
+    learned_xs[:,1],
+    learned_xs[:,2],
+    lw=0.75
+)
+
+ax1.set_title('True Dynamics')
+ax2.set_xlabel('x_1')
+ax2.set_ylabel('x_2')
+ax2.set_zlabel('x_3')
+ax1.set_xlim(-20.0, 20.0)
+ax1.set_ylim(-50.0, 50.0)
+ax1.set_zlim(0.0, 50.0)
+
+ax2.set_title('Learned Dynamics')
+ax2.set_xlabel('x_1')
+ax2.set_ylabel('x_2')
+ax2.set_zlabel('x_3')
+ax2.set_xlim(-20.0, 20.0)
+ax2.set_ylim(-50.0, 50.0)
+ax2.set_zlim(0.0, 50.0)
+
+lines_labels = [ax1.get_legend_handles_labels()]
 lines, labels = [sum(lol, []) for lol in zip(*lines_labels)]
 fig.legend(lines, labels)
 
