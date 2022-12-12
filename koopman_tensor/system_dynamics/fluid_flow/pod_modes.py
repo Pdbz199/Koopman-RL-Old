a66e5c1ab5919b4afc68c4e7a546c4e8ef1d307a
Preston Rozwood <279pdbz199@gmail.com>
2022-12-12 01:17:01 -0800

    Recreating fluid flow video

--- koopman_tensor/system_dynamics/fluid_flow/pod_modes.py
+++ koopman_tensor/system_dynamics/fluid_flow/pod_modes.py
@@ -1,19 +1,45 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.io
 
-alpha = np.load('data/ALPHA.npy') # (17000, 8)
-alpha_s = np.load('data/ALPHA-S.npy') # (17000, 8)
-phi = np.load('data/VORT-PHI.npy') # (89351, 41)
-avg = np.load('data/VORT-AVG.npy') # (89351, 1)
-delta = np.load('data/VORT-DELTA.npy') # (89351, 1)
+from matplotlib.animation import FuncAnimation
 
-# for k=100:100:length(alpha(:,1))
+p = scipy.io.loadmat('data/POD-COEFFS.mat')
+alpha = p['alpha'] # (17000, 8)
+alphaS = p['alphaS'] # (17000, 1)
+p2 = scipy.io.loadmat('data/POD-MODES.mat')
+Xavg = p2['Xavg'] # (89351, 1)
+Xdelta = p2['Xdelta'] # (89351, 1)
+Phi = p2['Phi'] # (89351, 8)
+
+fps = 30
+nSeconds = 5
+
+# snapshots = [ np.random.rand(5,5) for _ in range( nSeconds * fps ) ]
+snapshots = []
 for k in range(100, alpha.shape[0], 100):
-    k = 0
-    # u = Xavg + Phi(:,1)*alpha(k,1) + Phi(:,2)*alpha(k,2) + Xdelta*alphaS(k);
-    u = avg + phi[:,0] * alpha[k,0] + phi[:,1] * alpha[k,1] + delta * alpha_s[k]
-    # imshow(reshape(u,199,449)); colormap hot; caxis([-1 1])
-    plt.imshow(u.reshape(199,499), cmap='hot', caxis=(-1,1))
-    plt.show()
-    # pause(0.01)
-    plt.pause(0.01)
+    u = Xavg[:,0] + Phi[:,0] * alpha[k,0] + Phi[:,1] * alpha[k,1] + Xdelta[:,0] * alphaS[k,0]
+    snapshots.append(u.reshape(449,199).T)
+    # plt.imshow(u.reshape(449,199).T, cmap='hot', clim=(-1,1))
+
+# First set up the figure, the axis, and the plot element we want to animate
+fig = plt.figure( figsize=(8,8) )
+
+a = snapshots[0]
+im = plt.imshow(a, cmap='hot', clim=(-1,1))
+
+def animate_func(i):
+    if i % fps == 0:
+        print( '.', end ='' )
+
+    im.set_array(snapshots[i])
+    return [im]
+
+anim = FuncAnimation(
+    fig,
+    animate_func,
+    frames = nSeconds * fps,
+    interval = 1000 / fps # in ms
+)
+
+plt.show()
