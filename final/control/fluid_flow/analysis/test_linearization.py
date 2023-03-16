Fixed norm computation

final/control/fluid_flow/analysis/test_linearization.py
@@ -22,20 +22,20 @@ def linear_continuous_f(t, x, u):
     return continuous_A @ x + continuous_B @ u
 
 if __name__ == "__main__":
-    num_paths = 10_000
+    num_paths = 100
     num_time_units = 10
     num_steps_per_path = int(num_time_units / dt)
 
     nonlinearized_states = np.zeros((num_paths, num_steps_per_path, state_dim))
     linearized_states = np.zeros((num_paths, num_steps_per_path, state_dim))
 
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1, projection='3d')
+    # fig = plt.figure()
+    # ax = fig.add_subplot(1, 1, 1, projection='3d')
 
     # On limit cycle
-    # initial_states = get_random_initial_conditions(num_samples=num_paths)
+    initial_states = get_random_initial_conditions(num_samples=num_paths)
     # Fixed point
-    initial_states = np.ones((num_paths, state_dim)) * 0.05
+    # initial_states = np.ones((num_paths, state_dim)) * 0.05
     # Random points
     # initial_states = np.random.uniform(
     #     state_minimums,
@@ -44,29 +44,88 @@ if __name__ == "__main__":
     # ).T
 
     # Compute state_dots and state_dot_hats
-    state_dots = np.zeros((num_paths, state_dim))
-    state_dot_hats = np.zeros((num_paths, state_dim))
+    state_dots = np.zeros((num_paths, num_steps_per_path, state_dim))
+    state_dot_hats = np.zeros((num_paths, num_steps_per_path, state_dim))
     for path_num in range(num_paths):
         state = np.vstack(initial_states[path_num])
-        zero_action = zero_policy(state)
+        for step_num in range(num_steps_per_path):
+            zero_action = zero_policy(state)
 
-        # x_dot from true dynamics
-        state_dot = continuous_f(zero_action[0, 0])(0, state[:, 0])
-        state_dots[path_num] = state_dot
+            # x_dot from true dynamics
+            state_dot = continuous_f(zero_action[0, 0])(0, state[:, 0])
+            state_dots[path_num, step_num] = state_dot
 
-        # x_dot_hat from linearized dynamics
-        state_dot_hat = linear_continuous_f(0, state, zero_action)
-        state_dot_hats[path_num] = state_dot_hat[:, 0]
+            # x_dot_hat from linearized dynamics
+            state_dot_hat = linear_continuous_f(0, state, zero_action)
+            state_dot_hats[path_num, step_num] = state_dot_hat[:, 0]
 
     # Compute norms between derivative estimates
-    state_dot_difference_norms = np.linalg.norm(state_dots - state_dot_hats, axis=1)
+    state_dot_difference_norms = np.linalg.norm(state_dots - state_dot_hats, axis=2) # (path by step)
     average_state_dot_difference_norm = state_dot_difference_norms.mean()
+    print()
     print(
         "Average (state_dot - state_dot_hat) norm:",
         average_state_dot_difference_norm
     )
-    average_state_dot = state_dots.mean()
+    average_state_dot_norm = np.linalg.norm(state_dots, axis=2).mean() # (path by step)
     print(
         "Average (state_dot - state_dot_hat) norm normalized by average state_dot norm:",
-        average_state_dot_difference_norm / average_state_dot
+        average_state_dot_difference_norm / average_state_dot_norm
     )
+
+    """ x_0 to x_t """
+
+    nonlinearized_states = np.zeros((num_paths, num_steps_per_path, state_dim))
+    linearized_states = np.zeros((num_paths, num_steps_per_path, state_dim))
+
+    for path_num in range(num_paths):
+        nonlinearized_state = np.vstack(initial_states[path_num])
+
+        zero_action = np.array([[0]])
+
+        for step_num in range(num_steps_per_path):
+            nonlinearized_states[path_num, step_num] = nonlinearized_state[:, 0]
+            nonlinearized_state = f(nonlinearized_state, zero_action)
+
+        linearized_states[path_num] = odeint(
+            linear_continuous_f,
+            initial_states[path_num],
+            t=np.arange(0, num_time_units, dt),
+            args=(zero_action[:, 0],),
+            tfirst=True
+        )
+
+    # Compute norms between true states and estimated states
+    state_difference_norms = np.linalg.norm(nonlinearized_states - linearized_states, axis=2)
+    average_state_difference_norm = state_dot_difference_norms.mean()
+    print()
+    print(
+        "Average (nonlinearized state - linearized state) norm:",
+        average_state_difference_norm
+    )
+    average_nonlinearized_state = np.linalg.norm(nonlinearized_states, axis=2).mean()
+    print(
+        "Average (nonlinearized state - linearized state) norm normalized by average nonlinearized state norm:",
+        average_state_difference_norm / average_nonlinearized_state
+    )
+
+    # Plot an example trajectory
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1, projection='3d')
+
+    ax.set_xlim(-1, 1)
+    ax.set_ylim(-1, 1)
+    ax.set_zlim(0, 1)
+
+    episode_num_to_plot = 0
+    ax.plot3D(
+        nonlinearized_states[episode_num_to_plot, :, 0],
+        nonlinearized_states[episode_num_to_plot, :, 1],
+        nonlinearized_states[episode_num_to_plot, :, 2]
+    )
+    ax.plot3D(
+        linearized_states[episode_num_to_plot, :, 0],
+        linearized_states[episode_num_to_plot, :, 1],
+        linearized_states[episode_num_to_plot, :, 2]
+    )
+    plt.show()